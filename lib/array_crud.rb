918e26e0f4bf66aaa4da2e74cc58b168d0c4840c
Junko Tahara <junkotahara911@gmail.com>
2020-03-15 01:55:28 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,7 +23,8 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  array[index_number]
+  result = array[index_number]
+  result
 end
 
 def retrieve_first_element_from_array(array)
